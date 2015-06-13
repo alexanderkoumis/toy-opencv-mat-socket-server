3dfc558230eedb50732f376ac903252c87f3f1db
akoumis <alexander@visidyn.com>
2015-06-13 13:32:54 -0700

    removed globals

--- src/sample/main.cpp
+++ src/sample/main.cpp
@@ -6,14 +6,9 @@
 #include <future>     // future, async
 #include <iostream>
 
-std::once_flag once_dims;
-std::once_flag once_serv;
-std::unique_ptr<SocketClient> client_ptr;
-std::unique_ptr<SocketServer> server_ptr;
-
-std::thread send_thread;
-
-int ClientServerConnect(const std::string hostname, const int port) {
+int ClientServerConnect(const std::string hostname, const int port,
+                        std::unique_ptr<SocketClient>& client_ptr,
+                        std::unique_ptr<SocketServer>& server_ptr) {
 
   SocketClient::SockInfo_t client_info;
   SocketServer::SockInfo_t server_info;
@@ -55,7 +50,9 @@ int ClientServerConnect(const std::string hostname, const int port) {
   return 0;
 }
 
-void TransmitDims(const cv::Mat& image, cv::Size2i& image_dims) {
+void TransmitDims(const std::unique_ptr<SocketClient>& client_ptr,
+                  const std::unique_ptr<SocketServer>& server_ptr,
+                  const cv::Mat& image, cv::Size2i& image_dims) {
 
   std::future<cv::Size2i> s_recv_dims = std::async(std::launch::async, [&]() {
     return server_ptr->ReceiveImageDims();
@@ -73,7 +70,8 @@ void TransmitDims(const cv::Mat& image, cv::Size2i& image_dims) {
 
 void LaunchServer(const std::unique_ptr<SocketServer>& server_ptr,
                   const cv::Size2i& image_dims,
-                  const std::string& out_dir) {
+                  const std::string& out_dir,
+                  std::thread& send_thread) {
   static int pic_num = 0;
   std::packaged_task<void()> t_serv_init ([&](){
     while (1) {
@@ -87,20 +85,32 @@ void LaunchServer(const std::unique_ptr<SocketServer>& server_ptr,
   send_thread = std::thread(std::move(t_serv_init));
 }
 
-void TransmitImage(const cv::Mat& image, cv::Size2i& image_dims,
-                   const std::string out_dir) {
+void TransmitImage(const std::unique_ptr<SocketClient>& client_ptr,
+                   const std::unique_ptr<SocketServer>& server_ptr,
+                   const cv::Mat& image, cv::Size2i& image_dims,
+                   const std::string out_dir,
+                   std::thread& send_thread) {
 
-  std::call_once(once_dims, TransmitDims, std::cref(image),
+  static std::once_flag once_dims;
+  static std::once_flag once_serv;
+  std::call_once(once_dims, TransmitDims, std::cref(client_ptr),
+                                          std::cref(server_ptr),
+                                          std::cref(image),
                                           std::ref(image_dims));
   std::call_once(once_serv, LaunchServer, std::cref(server_ptr),
                                           std::cref(image_dims),
-                                          std::ref(out_dir));
+                                          std::cref(out_dir),
+                                          std::ref(send_thread));
   client_ptr->SendImage(image);
 }
 
 int main(int argc, char** argv) {
 
   setbuf(stdout, NULL);
+
+  std::unique_ptr<SocketClient> client_ptr;
+  std::unique_ptr<SocketServer> server_ptr;
+  std::thread send_thread;
 
   std::vector<cv::Mat> images;
   cv::Size2i image_dims;
@@ -114,8 +124,7 @@ int main(int argc, char** argv) {
 
   LoadImages(in_dir, images);
 
-
-  if (ClientServerConnect(hostname, port) == -1) {
+  if (ClientServerConnect(hostname, port, client_ptr, server_ptr) == -1) {
     printf("Error creating socket client/server pair\n");
     return 1;
   }
@@ -129,7 +138,7 @@ int main(int argc, char** argv) {
   for (auto& image : images) {
     static int num = 0;
     printf("Sending image %d/%zu\n", num++, images.size());
-    TransmitImage(image, image_dims, out_dir);
+    TransmitImage(client_ptr, server_ptr, image, image_dims, out_dir, send_thread);
   }
 
   if (send_thread.joinable()) {
